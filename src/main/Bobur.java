efbcd06248ae59da6dfadb7b2ad43d9c9da3b2cd
shavaju@gmail.com <shavaju@gmail.com>
2018-03-10 13:25:58 -0500

    added another print statement

--- src/main/Bobur.java
+++ src/main/Bobur.java
@@ -5,6 +5,7 @@ public class Bobur {
 	public static void main(String[] args) {
 		System.out.println("Hi EveryOne!!!");
 		System.out.println("Hi Bobur ,Muhammt ");
+		System.out.println("Hi Bobur ,ishlar qaley endi :)");
 	} 
 	
 }
